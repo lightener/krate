01ab5bbfd061c4928b802b416b647eb180b21890
Emory Taylor <never1ess@hotmail.com>
2014-03-10 13:54:30 -0700

    Krate gem first version

---
+++ krate_spec.rb
@@ -0,0 +1,19 @@
+require './Krate.rb'
+
+describe Krate do
+
+	it "should count the files processed" do
+		total = Krate.run
+		total.should eq 3 #3 files in directory
+	end
+
+	it "should store the song info" do
+		song = ID3Tag.new('song', 'test') #creates a new instance of ID3Tag
+		song.title.should == ('test')
+	end
+
+	it "should rename the mp3 files" do
+		songs = Dir.glob('*.mp3')
+		songs.should include("Knight Riderz - Synthetic Seduction.mp3") 
+    end
+end
